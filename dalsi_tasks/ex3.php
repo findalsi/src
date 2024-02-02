fa26b2af08508c013313ff59385827d1f0319ddc
Dalsi Samarathunga <findalsidu@gmail.com>
2024-02-02 11:02:09 +0200

    add repo

---
+++ dalsi_tasks/ex3.php
@@ -0,0 +1,94 @@
+<?php
+$title = "Exercise 3: Variable, Strings & Operators:";
+include "header.php"; ?>
+
+<h2>2. Form Creation: Create a simple HTML form to collect the user’s Firstname and Lastname. 
+    Use the echo statement to print “Hello [Firstname] [Lastname], You are welcome to my site.” inside an h3 tag. </h2>
+<h2>3.Bootstrap Styling: Apply Bootstrap styles to the form. 
+    Make sure to include the Bootstrap CSS link in your head tag.</h2>
+<?php
+echo "<h3>Hello [Firstname] [Lastname], You are welcome to my site.</h3>";
+?>
+
+
+    
+<form>
+    <div class="form1">
+        <label for="fnimi">Firstname:</label>
+        <input type="text" class="form-control" id="fnimi" name="fnimi">
+    </div>
+
+    <div class="form1">
+        <label for="lnimi">Lastname:</label>
+        <input type="text" class="form-control" id="lnimi" name="lnimi">
+    </div>
+
+<h2>4. HTML Table: Prepare a simple HTML table and apply Bootstrap styles to it. 
+    Note, you have already done a table in Exercise 1. 
+    If you wish, you can use the same table.</h2>
+
+    <?php
+$g1 = 5;
+$g2 = 4;
+$g3 = 5;
+?>
+
+
+    <table class="table table-striped table-bordered">
+    <thead class="thead-light">
+<tr><th>S.n</th><th>Name</th><th>Grade</th></tr>
+<tr>
+    <td>1</td>
+    <td>John</td>
+    <td><?php echo $g1 ?></td>
+
+</tr>
+
+<tr>
+    <td>2</td>
+    <td>Alice</td>
+    <td><?php echo $g2 ?></td>
+
+</tr>
+
+<tr>
+    <td>3</td>
+    <td>Bob</td>
+    <td><?php echo $g3 ?></td>
+
+</tr>
+</table>
+
+
+<h2>5. String Variables: Write a PHP script with two string variables 
+    ($str1 = "Hello"; $str2 = "World";). Join them together and print the length of the string.</h2>
+<?php
+$str1="Hello";
+$str2="World";
+
+echo $str1 . " " . $str2 . "<br>";
+echo "Length of the String: " . strlen($str1 . " " . $str2);
+?>
+
+<h2>6. Number Addition: Write a script to add up the numbers: 298, 234, 46. 
+    Use variables to store these numbers and an echo statement to output your answer.</h2>
+
+    <?php
+
+$first_number = 298;
+$second_number = 234;
+$third_number = 46;
+$sum_total = $first_number + $second_number + $third_number;
+
+$direct_text = 'Result = ';
+
+print ($direct_text . $sum_total);
+
+?>
+
+<h2>Browser Detection: Write a PHP script to detect the browser being used to view your pages. 
+    Hint: Use predefined variables: $_SERVER). Predefined Variables</h2>
+
+
+
+<?php include "footer.php"; ?>
--- dalsi_tasks/header.php
+++ dalsi_tasks/header.php
@@ -5,6 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo $title; ?></title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
     <link rel="stylesheet" href="styles.css">
 </head>
 <body>
@@ -17,7 +19,7 @@
           <nav>
             <a href="ex1.php">Ex1</a>
             <a href="new.php">New</a>
-            <a href="contactus.php">Contact Us</a>
+            <a href="ex3.php">Ex3</a>
           </nav>
         </div>
       </div>
